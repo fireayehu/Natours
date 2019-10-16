607a59737a88343b4c51926c5b174d9e7a0d47df
Fireayehu Zekarias <fireayehuzekarias1989@gmail.com>
2019-10-16 18:23:06 +0300

    serving static files

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 //Middlewares
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(express.static(`${__dirname}/public`));
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
