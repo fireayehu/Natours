bcaf3fb03cab201e6be396116487c389aa1da0f6
Fireayehu Zekarias <fireayehuzekarias1989@gmail.com>
2019-10-16 16:45:48 +0300

    refractoring to contollers, routes and sever.js

--- app.js
+++ app.js
@@ -1,127 +1,15 @@
-const fs = require('fs');
 const express = require('express');
 const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoute');
+const userRouter = require('./routes/userRoute');
+
 const app = express();
 
 //Middlewares
 app.use(express.json());
 app.use(morgan('dev'));
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
-//File Storage
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-
-//Route Handlers
-
-const getAllTours = (req, res) => {
-  res
-    .status(200)
-    .json({ status: 'success', results: tours.length, data: { tours } });
-};
-const createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const tour = Object.assign({ id: newId }, req.body);
-  tours.push(tour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    err => {
-      res.status(201).json({
-        status: 'success',
-        data: { tour }
-      });
-    }
-  );
-};
-const getTour = (req, res) => {
-  const id = parseInt(req.params.id, 10);
-  const tour = tours.find(tour => tour.id === id);
-  if (!tour) {
-    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
-  }
-  res.status(200).json({ status: 'success', data: { tour } });
-};
-
-const updateTour = (req, res) => {
-  const id = parseInt(req.params.id, 10);
-  const tour = tours.find(tour => tour.id === id);
-  if (!tour) {
-    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
-  }
-  res.status(200).json({
-    status: 'success',
-    data: { tour: '<Updated tour here...>' }
-  });
-};
-
-const deleteTour = (req, res) => {
-  const id = parseInt(req.params.id, 10);
-  const tour = tours.find(tour => tour.id === id);
-  if (!tour) {
-    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
-  }
-  res.status(204).json({
-    status: 'success',
-    data: null
-  });
-};
-
-const getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This Api end point is not ready'
-  });
-};
-const createUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This Api end point is not ready'
-  });
-};
-const getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This Api end point is not ready'
-  });
-};
-const updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This Api end point is not ready'
-  });
-};
-const deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This Api end point is not ready'
-  });
-};
-
-app
-  .route('/api/v1/tours')
-  .get(getAllTours)
-  .post(createTour);
-
-app
-  .route('/api/v1/tours/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
-
-app
-  .route('/api/v1/users')
-  .get(getAllUsers)
-  .post(createUser);
-
-app
-  .route('/api/v1/users/:id')
-  .get(getUser)
-  .patch(updateUser)
-  .delete(deleteUser);
-
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Application Running at port ${port}...`);
-});
+module.exports = app;
