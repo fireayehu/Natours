d183a41b4a55a5b267008a546358b946639b4f6d
Fireayehu Zekarias <fireayehuzekarias1989@gmail.com>
2019-10-16 14:39:23 +0300

    natours express tour app

---
+++ app.js
@@ -0,0 +1,73 @@
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+//Middlewares
+app.use(express.json());
+
+//Apis
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+app.get('/api/v1/tours', (req, res) => {
+  res
+    .status(200)
+    .json({ status: 'success', results: tours.length, data: { tours } });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  const tour = tours.find(tour => tour.id === id);
+  if (!tour) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  }
+  res.status(200).json({ status: 'success', data: { tour } });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const tour = Object.assign({ id: newId }, req.body);
+  tours.push(tour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      res.status(201).json({
+        status: 'success',
+        data: { tour }
+      });
+    }
+  );
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  const tour = tours.find(tour => tour.id === id);
+  if (!tour) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: { tour: '<Updated tour here...>' }
+  });
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  const tour = tours.find(tour => tour.id === id);
+  if (!tour) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Application Running at port ${port}...`);
+});
