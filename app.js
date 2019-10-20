45c72bb766e3f6da62ccfcf9f42808147f575c3f
Fireayehu Zekarias <fireayehuzekarias1989@gmail.com>
2019-10-20 10:15:24 +0300

    environmental variables

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ const userRouter = require('./routes/userRoute');
 const app = express();
 
 //Middlewares
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
-app.use(morgan('dev'));
 app.use(express.static(`${__dirname}/public`));
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
