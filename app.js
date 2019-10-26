11589d480ea90231ed2c706b81395b1da9bc9e92
Fireayehu Zekarias <fireayehuzekarias1989@gmail.com>
2019-10-26 14:19:43 +0300

    express security

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const mongoSanitize = require('express-mongo-sanitize');
+const hpp = require('hpp');
 const AppError = require('./utils/appError');
 const { errorHandler } = require('./controllers/errorController');
 
@@ -10,11 +14,34 @@ const userRouter = require('./routes/userRoute');
 const app = express();
 
 //Middlewares
+
+app.use(helmet());
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-app.use(express.json());
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again after an hour'
+});
+app.use(express.json({ limit: '10kb' }));
 app.use(express.static(`${__dirname}/public`));
+app.use('/api', limiter);
+app.use(mongoSanitize());
+app.use(xss());
+app.use(
+  hpp({
+    whitelist: [
+      'ratingsAverage',
+      'ratingsQuantity',
+      'duration',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
--- controllers/authController.js
+++ controllers/authController.js
@@ -33,6 +33,8 @@ exports.login = catchAsync(async (req, res, next) => {
 
 exports.signup = catchAsync(async (req, res, next) => {
   const user = await User.create(req.body);
+
+  jwt.coo;
   const token = getToken(user._id);
   res.status(201).json({
     status: 'success',
