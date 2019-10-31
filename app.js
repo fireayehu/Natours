167c76c2311546f919fe1dc1a08a2ca126e68807
Fireayehu Zekarias <fireayehuzekarias1989@gmail.com>
2019-10-31 19:00:08 +0300

    working with pug and server side rendering

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -11,11 +12,14 @@ const { errorHandler } = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoute');
 const userRouter = require('./routes/userRoute');
 const reviewRouter = require('./routes/reviewRoute');
+const viewRouter = require('./routes/viewRoute');
 
 const app = express();
 
 //Middlewares
-
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(helmet());
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -26,8 +30,8 @@ const limiter = rateLimit({
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again after an hour'
 });
+
 app.use(express.json({ limit: '10kb' }));
-app.use(express.static(`${__dirname}/public`));
 app.use('/api', limiter);
 app.use(mongoSanitize());
 app.use(xss());
@@ -43,9 +47,12 @@ app.use(
     ]
   })
 );
+//Routes
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
+
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on the server!`, 404));
 });
